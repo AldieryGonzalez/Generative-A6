fad80c8236e0b982c8a280beff37334d6958639d
AldieryGonzalez <aldieryrgon@gmail.com>
2022-11-22 22:08:12 -0600

    Commit

--- README.md
+++ README.md
@@ -1,64 +1,56 @@
 # A6: Chatbot
 
 ## Your name
+
 Aldiery Gonzalez
 
 ## Your Glitch link
-[my page](https://aldiery-a1.glitch.me)
 
+[my page](https://aldiery-a1.glitch.me)
 
 ## Describe your bot's character
 
---your answer here  --
+It is me, late for thanksgiving outside my parents house
 
 ## How does the user know what to say to this bot How are you solving the blank slate problem?
 
---your answer here  --
+The chips are a great help, but whenever I want the use to know there is a special command, I take the omniscient narrator route and quote somethins using \"
 
 ## How have your styled your page to best present this character? What scene or mood are you suggesting?
 
---your answer here  --
+I made it Holiday themed with a gif in the background for fall
 
-## Describe the first thing this bot can respond to.  What is the expressive range of what you say in response?
+## Describe the first thing this bot can respond to. What is the expressive range of what you say in response?
 
---your answer here  --
-
+You are at the door, you can try to leave, knock or wait, and the bot will guid you
 
 ## Describe the second thing this bot can respond to
 
---your answer here  --
-
+it branches off a lot after the first response but you can be at dinner or talking to your mom, in which case you will end up at dinner
 
 ## Describe the third thing this bot can respond to
 
---your answer here  --
-
+Describe your family and their changing favorite meals
 
 ## Describe the fourth thing this bot can respond to
 
---your answer here  --
-
+Fight and end thanksgiving
 
 ## Describe the fifth thing this bot can respond to
 
---your answer here  --
-
-
+Try to run away from it all
 
 ## What states does this bot move through? Is this a common social script? Why do the states connect like that?
 
---your answer here  --
-
-
+It tries to be a text adventure more or less, or more an intro to them
 
 ## In what ways does your bot obey or subvert the Gricean maxims
 
---your answer here  --
+--your answer here --
 
 ## What 3 other bots did you look at? What was interesting, notable or useful about them?
 
---your answer here  --
-
+--your answer here --
 
 ## List any resources (code, images, etc) you've used, and where you got them from
 
--- bot-maps.js
+++ bot-maps.js
@@ -1,184 +1,382 @@
 const BOT_MAPS = {
-  // A different brain, this one is for EMOJI
-  emojiBot: {
-    title: "Only speaks emoji",
-    botPfp: "😬",
-    humanPfp: "🌶",
-    chips: ["😸", "🍞", "👋"],
+	homeAlone: {
+		title: "Only speaks emoji",
+		botPfp: "🦃",
+		humanPfp: "🥱",
+		chips: ["⬆️", "⬇️", "➡️", "⬅️", "⚔️"],
+		// TWO BIG THINGS: STATES, and GRAMMAR
 
-    // TWO BIG THINGS: STATES, and GRAMMAR
+		// Our Finite State Machine
+		states: {
+			// The state we start at
+			origin: {
+				// When we enter the state say this
+				onEnterSay: ["You are late for Thanksgiving at your parents place"],
+				exits: [
+					"'⬆️' -> origin 'Maybe \"knock\" before going in'",
+					"'⬇️' -> origin 'Maybe \"knock\" before going in'",
+					"'➡️' -> origin 'Maybe \"knock\" before going in'",
+					"'⬅️' -> origin 'Maybe \"knock\" before going in'",
+					"'⚔️' -> origin 'Maybe \"knock\" before going in'",
+					"'knock' -> knock '(Distant) Honey get the door.'",
+					"wait:7 ->awkMomConversation 'Somebody opens the door'",
+				],
 
-    // Our Finite State Machine
-    states: {
-      // The state we start at
-      origin: {
-        // When we enter the state say this
-        onEnterSay: ["I'm a bot #hello#"],
-        exits: [
-          // Exits have three things: conditions ->target actions
-          // "wait:random(5,7) ->@ '#emoji##emoji##emoji#'",
+				// onExitSay: ["Good luck!"],
+			},
 
-          // Under what conditions can I take this exit?
-          // 'stuff' take this exit if the user says "stuff"
-          // '*' or says ANYTHING
-          // Target: name of a state, or "@" go back in here
-          // "'*' ->@ 'OOPs'",
+			awkMomConversation: {
+				exits: [
+					"'⬆️' -> tryKnock 'Maybe \"knock\" before going in'",
+					"'⬇️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'➡️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'⬅️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'⚔️' -> end",
+					"wait:8 -> awkMomConversation '#tooLong#'",
+				],
+			},
+			knock: {
+				exits: [
+					"'⬆️' -> dinner ''",
+					"'⬇️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'➡️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'⬅️' -> cantLeave 'Nope, go forward, INTO the house'",
+					"'⚔️' -> end",
+					"wait:8 -> awkMomConversation '#tooLong#'",
+				],
+			},
+			dinner: {
+				exits: [
+					"'⬆️' -> dinner '#family# is peacefully eating their favorite, #food#'",
+					"'⬇️' -> dinner '#family# is peacefully eating their favorite, #food#'",
+					"'➡️' -> dinner '#family# is peacefully eating their favorite, #food#'",
+					"'⬅️' -> dinner '#family# is peacefully eating their favorite, #food#'",
+					"'⚔️' -> end 'You are so bored you throw down with #family#'",
+					"wait:8 -> awkMomConversation '#tooLong#'",
+				],
+			},
+			cantLeave: {
+				onEnterSay: ["You got this, get your head in the game, you join your"],
+				exits: ["wait:0.5 -> dinner"],
+			},
 
-          // Wait 2 seconds
-          "wait:2 ->conversation '⏳ going to conversation mode'",
-        ],
+			end: {
+				onEnterSay: ["You fought the good fight... and lost, the end"],
+			},
+		},
 
-        // onExitSay: ["Good luck!"],
-      },
+		// GRAMMAR!!!
+		grammar: {
+			hello: ["Hey dear", "(She is delighted to see you)"],
+			tooLong: [
+				"You just gonna stand there? Come in dear",
+				"Oh honey, youre home!",
+			],
+			cantLeave: [
+				"You cant run away from all of your problems, now come ere",
+				"The food smells too good to leave",
+				"Youre already late, you cant go now",
+			],
+			food: [
+				"Cranberry Sauce",
+				"Turkey",
+				"Chilli",
+				"Sprite Cranbery",
+				"More Turey",
+			],
+			family: ["Lin Nancy", "Big Nancy", "Robert (The Great)", "Mom", "Dad"],
+		},
+	},
+	// A different brain, this one is for EMOJI
+	emojiBot: {
+		title: "Only speaks emoji",
+		botPfp: "😬",
+		humanPfp: "🌶",
+		chips: ["😸", "🍞", "👋"],
 
-      conversation: {
-        exits: ["'👋' ->end '😭'", "'*' ->@ '#emoji#'"],
-      },
+		// TWO BIG THINGS: STATES, and GRAMMAR
 
-      end: {
-        onEnterSay: ["the end"],
-      },
-    },
+		// Our Finite State Machine
+		states: {
+			// The state we start at
+			origin: {
+				// When we enter the state say this
+				onEnterSay: ["I'm a bot #hello#"],
+				exits: [
+					// Exits have three things: conditions ->target actions
+					// "wait:random(5,7) ->@ '#emoji##emoji##emoji#'",
 
-    // GRAMMAR!!!
-    grammar: {
-      hello: ["👋", "😀"],
-      story: ["#emoji# #emoji# #emoji# story"],
-      emoji: ["#animal#", "#food#", "#heart#"],
-      animal: ["🐧", "🐈", "🦒", "🐕", "🐿", "🐓", "🐁"],
-      food: ["🍊", "🥞", "🥨", "🧀", "🌽", "🌶", "🍏"],
-      heart: ["💕", "💜", "💙", "💔"],
-    },
-  },
+					// Under what conditions can I take this exit?
+					// 'stuff' take this exit if the user says "stuff"
+					// '*' or says ANYTHING
+					// Target: name of a state, or "@" go back in here
+					// "'*' ->@ 'OOPs'",
 
-  hauntedHouse: {
-    title: "Only speaks emoji",
-    botPfp: "🏚",
-    humanPfp: "😬",
-    chips: ["N", "E", "W", "S"],
+					// Wait 2 seconds
+					"wait:2 ->conversation '⏳ going to conversation mode'",
+				],
 
-    states: {
-      origin: {
-        onEnterSay: ["You are in a spooky house, you hear scary sounds to the east", 'some music starts <iframe width="560" height="315" src="https://www.youtube.com/embed/Z6ylGHfLrdI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'],
-        
-        exits: ["wait:20 ->died",
-               "'N' ->room 'You explore north'",
-               "'E' ->room 'You explore east'",
-               "'W' ->room 'You explore west'",
-               "'S' ->room 'You explore south'",
-               ]
-      },
-      
-       
-      room: {
-        onEnterSay: ["You are in a #roomAdjective# #roomType#"],
-        exits: [
-           "'N' ->room 'You explore north'",
-               "'E' ->fight 'You see a #monster#'",
-               "'W' ->room 'You explore west'",
-               "'S' ->room 'You explore south'",
-              "'look' ->@ '#spookyDiscovery#'"
-        ]
-      },
-      
-       fight: {
-        onEnterSay: ["You lose the fight"],
-         exits: [
-          "wait:2 ->died",
-          ]
-      },
-      
-      died: {
-        onEnterSay: ["You died", "☠️"],
-      }
-    },
-    
-    grammar: {
-      object: ["kettle", "table", "chair", "desk", "lamp", "vase", "urn", "candelabra", "lantern", "idol", "orb", "book", "basket", "hammer", "flowerpot", "bicycle", "paintbrush", "goblet", "bottle", "jar", "toaster", "teacup", "teapot", "rug","basket", "thimble", "ottoman", "cushion", "pen", "pencil", "mug","egg", "chair", "sun", "cloud", "bell", "bucket", "lemon", "glove", "moon", "star", "seed", "card", "pancake", "waffle", "car", "train", "spoon", "fork", "potato"],
-	  objAdj: ["wooden","old","vintage","woven", "antique","broken","tiny", "giant", "little", "upside-down","dented","imaginary","glowing","curséd","glittery","organic", "rusty", "multi-layered", "complicated", "ornate", "dusty", "gleaming", "fresh", "ancient", "forbidden", "milky", "upholstered", "comfortable", "dynamic", "solar-powered", "coal-fired", "warm", "cold", "frozen", "melted", "boxy", "well-polished", "vivid", "painted", "embroidered", "enhanced", "embellished", "collapsible", "simple", "demure"],
-	
-      spookyDiscovery: ["You find something scary:#objAdj# #object#"],
-      roomType: ["living room", "bedroom", "conservatory", "cemetary", "kitchen"],
-      roomAdjective: ["dusty", "abandoned", "blood-soaked", "ominous", "suspiciously normal"]
-    },
-  },
+				// onExitSay: ["Good luck!"],
+			},
 
-  myBot: {
-    title: "Cocoa-and-Therapy Bot",
-    description: [
-      "a bot for suggesting hot drinks and listening to your problems",
-    ],
+			conversation: {
+				exits: ["'👋' ->end '😭'", "'*' ->@ '#emoji#'"],
+			},
 
-    states: {
-      origin: {
-        onEnterSay:
-          "I'm your therapeutic cocoa machine. Tell me about your problems while I make you a nice warm drink",
-        exits: [
-          "'drink' ->makeDrink",
-          "'drink' ->makeDrink",
-          "'*' ->makeDrink",
-        ],
-      },
+			end: {
+				onEnterSay: ["the end"],
+			},
+		},
 
-      makeDrink: {
-        onEnterSay: "I'll make you a #drink#.",
-        exits: [
-          "wait:5 ->origin 'Ah, not quite the right time, I see.' 'Something else maybe?'",
-          "'something else' ->makeDrink 'How about something different then?'",
-          "* ->listen '*SLURP*'",
-        ],
-      },
+		// GRAMMAR!!!
+		grammar: {
+			hello: ["👋", "😀"],
+			story: ["#emoji# #emoji# #emoji# story"],
+			emoji: ["#animal#", "#food#", "#heart#"],
+			animal: ["🐧", "🐈", "🦒", "🐕", "🐿", "🐓", "🐁"],
+			food: ["🍊", "🥞", "🥨", "🧀", "🌽", "🌶", "🍏"],
+			heart: ["💕", "💜", "💙", "💔"],
+		},
+	},
 
-      listen0: {
-        onEnterSay: "#askAboutUser#",
-        exits: [
-          "wait:5 ->origin 'Quiet time is good too'",
-          "'*' ->origin '#sympathy#'",
-        ],
-      },
-      listen1: {
-        onEnterSay: ["#sympathy#", "#askAboutUser#"],
-      },
+	hauntedHouse: {
+		title: "Only speaks emoji",
+		botPfp: "🏚",
+		humanPfp: "😬",
+		chips: ["N", "E", "W", "S"],
 
-      exits: ["'*' -> '#sympathy#'"],
-    },
-    grammar: {
-      askAboutUser: [
-        "How was your day?",
-        "What's on your mind?",
-        "How is this week going?",
-      ],
-      listen: [
-        "mmhmm",
-        "tell me about it",
-        "tell me more?",
-        "does that happen often?",
-      ],
-      sympathy: [
-        "that sounds #difficult#",
-        "you've been through a lot",
-        "it sounds like you are trying very hard",
-      ],
-      difficult: ["challenging", "hard", "like a tough time"],
-      toppings: [
-        "caramel sauce",
-        "mini marshmallows",
-        "a candy cane",
-        "sprinkles",
-        "whipped cream",
-        "vegan whip",
-        "marshmallow fluff",
-        "grated nutmeg",
-      ],
-      milk: ["oatmilk", "soy", "whole milk", "skim", "almond milk"],
-      coffeeType: [
-        "latte",
-        "chai",
-        "espresso",
-        "frappe",
-        "mocha",
-        "hot chocolate",
-      ],
-    },
-  },
+		states: {
+			origin: {
+				onEnterSay: [
+					"You are in a spooky house, you hear scary sounds to the east",
+					'some music starts <iframe width="560" height="315" src="https://www.youtube.com/embed/Z6ylGHfLrdI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+				],
+
+				exits: [
+					"wait:20 ->died",
+					"'N' ->room 'You explore north'",
+					"'E' ->room 'You explore east'",
+					"'W' ->room 'You explore west'",
+					"'S' ->room 'You explore south'",
+				],
+			},
+
+			room: {
+				onEnterSay: ["You are in a #roomAdjective# #roomType#"],
+				exits: [
+					"'N' ->room 'You explore north'",
+					"'E' ->fight 'You see a #monster#'",
+					"'W' ->room 'You explore west'",
+					"'S' ->room 'You explore south'",
+					"'look' ->@ '#spookyDiscovery#'",
+				],
+			},
+
+			fight: {
+				onEnterSay: ["You lose the fight"],
+				exits: ["wait:2 ->died"],
+			},
+
+			died: {
+				onEnterSay: ["You died", "☠️"],
+			},
+		},
+
+		grammar: {
+			object: [
+				"kettle",
+				"table",
+				"chair",
+				"desk",
+				"lamp",
+				"vase",
+				"urn",
+				"candelabra",
+				"lantern",
+				"idol",
+				"orb",
+				"book",
+				"basket",
+				"hammer",
+				"flowerpot",
+				"bicycle",
+				"paintbrush",
+				"goblet",
+				"bottle",
+				"jar",
+				"toaster",
+				"teacup",
+				"teapot",
+				"rug",
+				"basket",
+				"thimble",
+				"ottoman",
+				"cushion",
+				"pen",
+				"pencil",
+				"mug",
+				"egg",
+				"chair",
+				"sun",
+				"cloud",
+				"bell",
+				"bucket",
+				"lemon",
+				"glove",
+				"moon",
+				"star",
+				"seed",
+				"card",
+				"pancake",
+				"waffle",
+				"car",
+				"train",
+				"spoon",
+				"fork",
+				"potato",
+			],
+			objAdj: [
+				"wooden",
+				"old",
+				"vintage",
+				"woven",
+				"antique",
+				"broken",
+				"tiny",
+				"giant",
+				"little",
+				"upside-down",
+				"dented",
+				"imaginary",
+				"glowing",
+				"curséd",
+				"glittery",
+				"organic",
+				"rusty",
+				"multi-layered",
+				"complicated",
+				"ornate",
+				"dusty",
+				"gleaming",
+				"fresh",
+				"ancient",
+				"forbidden",
+				"milky",
+				"upholstered",
+				"comfortable",
+				"dynamic",
+				"solar-powered",
+				"coal-fired",
+				"warm",
+				"cold",
+				"frozen",
+				"melted",
+				"boxy",
+				"well-polished",
+				"vivid",
+				"painted",
+				"embroidered",
+				"enhanced",
+				"embellished",
+				"collapsible",
+				"simple",
+				"demure",
+			],
+
+			spookyDiscovery: ["You find something scary:#objAdj# #object#"],
+			roomType: [
+				"living room",
+				"bedroom",
+				"conservatory",
+				"cemetary",
+				"kitchen",
+			],
+			roomAdjective: [
+				"dusty",
+				"abandoned",
+				"blood-soaked",
+				"ominous",
+				"suspiciously normal",
+			],
+		},
+	},
+
+	myBot: {
+		title: "Cocoa-and-Therapy Bot",
+		description: [
+			"a bot for suggesting hot drinks and listening to your problems",
+		],
+
+		states: {
+			origin: {
+				onEnterSay:
+					"I'm your therapeutic cocoa machine. Tell me about your problems while I make you a nice warm drink",
+				exits: [
+					"'drink' ->makeDrink",
+					"'drink' ->makeDrink",
+					"'*' ->makeDrink",
+				],
+			},
+
+			makeDrink: {
+				onEnterSay: "I'll make you a #drink#.",
+				exits: [
+					"wait:5 ->origin 'Ah, not quite the right time, I see.' 'Something else maybe?'",
+					"'something else' ->makeDrink 'How about something different then?'",
+					"* ->listen '*SLURP*'",
+				],
+			},
+
+			listen0: {
+				onEnterSay: "#askAboutUser#",
+				exits: [
+					"wait:5 ->origin 'Quiet time is good too'",
+					"'*' ->origin '#sympathy#'",
+				],
+			},
+			listen1: {
+				onEnterSay: ["#sympathy#", "#askAboutUser#"],
+			},
+
+			exits: ["'*' -> '#sympathy#'"],
+		},
+		grammar: {
+			askAboutUser: [
+				"How was your day?",
+				"What's on your mind?",
+				"How is this week going?",
+			],
+			listen: [
+				"mmhmm",
+				"tell me about it",
+				"tell me more?",
+				"does that happen often?",
+			],
+			sympathy: [
+				"that sounds #difficult#",
+				"you've been through a lot",
+				"it sounds like you are trying very hard",
+			],
+			difficult: ["challenging", "hard", "like a tough time"],
+			toppings: [
+				"caramel sauce",
+				"mini marshmallows",
+				"a candy cane",
+				"sprinkles",
+				"whipped cream",
+				"vegan whip",
+				"marshmallow fluff",
+				"grated nutmeg",
+			],
+			milk: ["oatmilk", "soy", "whole milk", "skim", "almond milk"],
+			coffeeType: [
+				"latte",
+				"chai",
+				"espresso",
+				"frappe",
+				"mocha",
+				"hot chocolate",
+			],
+		},
+	},
 };
